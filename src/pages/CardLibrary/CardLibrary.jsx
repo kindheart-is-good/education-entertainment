c08997c1a4941aedac08e42c1fc0efecc2da3606
metaphysical_light <metacat.me@gmail.com>
2023-12-07 16:03:35 +0300

    Card Library: Edited functions createCard, editCardById and deleteCardById so that they work not only with local state, but with sending requests to the server (JSON Server). Created new function fetchCards.

--- src/pages/CardLibrary/CardLibrary.jsx
+++ src/pages/CardLibrary/CardLibrary.jsx
@@ -1,49 +1,82 @@
-import React, {useState} from "react";
-import styles from "./CardLibrary.module.css"
-import CardCreate from "./componentsLocal/CardCreate"
+import React, { useEffect, useState } from "react";
+import styles from "./CardLibrary.module.css";
+import CardCreate from "./componentsLocal/CardCreate";
 import CardList from "./componentsLocal/CardList";
+import axios from "axios";
 
 const CardLibrary = () => {
+  const [cards, setCards] = useState([]);
 
-    const [cards, setCards] = useState([]);
+  useEffect(() => {
+    fetchCards();
+  }, []);
 
-    const editCardById = (id, newTitle) => {
-        const updatedCards = cards.map((card) => {
-            if (card.id === id) {
-                return { ...card, title: newTitle };
-            }
-            return card;
-        });
-        setCards(updatedCards);
-    };
+  const fetchCards = async () => {
+    const response = await axios.get(
+      process.env.REACT_APP_JSON_SERVER_URL + "/cards"
+    );
+    setCards(response.data);
+  };
 
-    const deleteCardById = (id) => {
-      const updatedCards = cards.filter((card) => {
-        return card.id !== id;
-      });
-      setCards(updatedCards);
-    };
+  const createCard = async (title) => {
+    //console.log('Need to add card with:', title);
 
-    const createCard = (title) => {
-        //console.log('Need to add card with:', title);
-        const updatedCards = [
-            ...cards,
-            {
-                id: Math.round(Math.random() * 9999),
-                title   // Эта запись тоже самое что и: { id: 123, title: title}
-            },
-        ];
-        setCards(updatedCards);
-    }
+    const response = await axios.post(
+        process.env.REACT_APP_JSON_SERVER_URL + "/cards",
+        {
+          title,
+        }
+    );
+    //console.log(response);
 
-    return (
-        /*<div>number of elements in cards array is: {cards.length}</div>*/
-        <div className={styles.content}>
-            <h1>List of Cards</h1>
-            <CardList cards={cards} onEdit={editCardById} onDelete={deleteCardById} />
-            <CardCreate onCreate={createCard} />
-        </div>
-    )
-}
+    const updatedCards = [
+      ...cards,
+      /*{
+          id: Math.round(Math.random() * 9999),
+          title
+        },*/
+      response.data,
+    ];
+    setCards(updatedCards);
+  };
+
+  const editCardById = async (id, newTitle) => {
+    const response = await axios.put(
+      process.env.REACT_APP_JSON_SERVER_URL + `/cards/${id}`,
+      {
+        title: newTitle,
+      }
+    );
+    //console.log(response);
+
+    const updatedCards = cards.map((card) => {
+      if (card.id === id) {
+        return { ...card, ...response.data };
+      }
+      return card;
+    });
+    setCards(updatedCards);
+  };
+
+  const deleteCardById = async (id) => {
+    await axios.delete(
+      process.env.REACT_APP_JSON_SERVER_URL + `/cards/${id}`
+    );
+
+    const updatedCards = cards.filter((card) => {
+      return card.id !== id;
+    });
+    setCards(updatedCards);
+  };
+
+  return (
+    /*<div>number of elements in cards array is: {cards.length}</div>*/
+    <div className={styles.content}>
+      <h1>List of Cards</h1>
+      <CardList cards={cards} onEdit={editCardById} onDelete={deleteCardById} />
+      <CardCreate onCreate={createCard} />
+    </div>
+  );
+};
 
 export default CardLibrary;
